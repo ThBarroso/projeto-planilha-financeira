bf83582e7bd9aeac540e0d150f5d79f219060171
ThiagoBarroso-Zoppy <thiago.barroso@zoppy.com.br>
2025-04-01 13:39:04 -0300

    atualização 01/04/2025 14hrs

--- src/componentes/Profile/index.js
+++ src/componentes/Profile/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import './Profile.css';
 import { MdOutlineAccountCircle } from "react-icons/md";
+import styled from 'styled-components';
+
+
+const ProfileContainer = styled.header`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin: 0 auto;
+  padding: 20px;
+  list-style: none;
+`;
+
+const ProfileLink = styled.a`
+  height: auto;
+  gap: 10px;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: #114934;
+  font-size: 20px;
+
+  &:hover {
+    color: #00b894;
+  }
+
+  span {
+    font-size: 14px;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
 
 
 function Profile() {
 return(
-    <div className="Profile">
-        <header className="Profile-header">
-            <div className="Profile-logo">
-                <li><a href="/"><MdOutlineAccountCircle /><span>Perfil</span></a></li>
-            </div>
-        </header>
-    </div>
+        <ProfileContainer>
+                <li><ProfileLink href="/"><MdOutlineAccountCircle /><span>Perfil</span></ProfileLink></li>
+        </ProfileContainer>
 );
 
 }
